ebb7621fdabaa0172d595e9fc0634c49bf0dc1b7
Luciano Bobsin <lucianosouzabobsin@gmail.com>
2021-03-28 00:25:59 -0300

    Adjust Verb to report list

--- runners/routes/api.php
+++ runners/routes/api.php
@@ -13,19 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('login', 'Auth\LoginController@login');
-
 Route::group(['middleware' => 'auth:api'], function() {
     Route::post('add.runner', 'RunnerController@store');
     Route::post('add.competition', 'CompetitionController@store');
+    Route::post('add.runner.competition', 'RunnerCompetitionController@store');
     Route::get('list.runner', 'RunnerController@index');
     Route::get('list.competition', 'CompetitionController@index');
-    Route::post('add.runner.competition', 'RunnerCompetitionController@store');
-    Route::post('report.get.list', 'ReportController@list');
+    Route::get('report.get.list', 'ReportController@list');
 });
 
